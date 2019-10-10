9595443f7ea1edea9d85eed8bf6f8bfd95c30dca
Martin Maier-Moessner <martin.maier.moessner@gmail.com>
2019-10-10 21:43:58 +0000

    empty

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const play = function(name) {
-  console.log(name);
   let audio = new Audio('https://raw.githubusercontent.com/tine2k/portal-combat-sound-board/master/' + name + '.mp3');
   audio.addEventListener('play', () => document.getElementById(name).disabled = true);
   audio.addEventListener('ended', () => document.getElementById(name).disabled = false);
@@ -12,7 +11,8 @@ document.getElementById('estimation').onclick = () => play('estimation');
 document.getElementById('kaffee').onclick = () => play('kaffee');
 document.getElementById('grooming').onclick = () => play('grooming');
 
-console.log('scheduling job... x');
-var scheduler = later.parse.text('at 11:43pm every weekday');
 later.date.localTime();
+var cronDef = 'at 11:43pm every weekday';
+console.log('scheduling job@' + cronDef);
+var scheduler = later.parse.text(cronDef);
 later.setInterval(() => play('standup'), scheduler);
