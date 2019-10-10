4ceff7eb089e4cf85300808d7565a5f7dcd8b685
tine2k <martin.maier.moessner@gmail.com>
2019-10-10 22:51:01 +0200

    3 files modified

--- index.ts
+++ index.ts
@@ -1,16 +1,21 @@
 // Import stylesheets
 import './style.css';
+import * as later from 'later';
 
 // Write TypeScript code!
-function play(name) {
+const play = function(name) {
   let audio = new Audio('https://raw.githubusercontent.com/tine2k/portal-combat-sound-board/master/' + name + '.mp3');
   audio.addEventListener('play', () => document.getElementById(name).disabled = true);
   audio.addEventListener('ended', () => document.getElementById(name).disabled = false);
   audio.play();
 }
 
-function cronjob() {
-  console.log('scheduling job...');
-  var textSched = later.parse.text('every 10 seconds');
-  var timer2 = later.setInterval(() => play('standup'), textSched);
-};
+document.getElementById('standup').onclick = () => play('standup');
+document.getElementById('review').onclick = () => play('review');
+document.getElementById('estimation').onclick = () => play('estimation');
+document.getElementById('kaffee').onclick = () => play('kaffee');
+document.getElementById('grooming').onclick = () => play('grooming');
+
+console.log('scheduling job...');
+var textSched = this.later.parse.text('every 10 seconds');
+var timer2 = later.setInterval(() => play('standup'), textSched);
