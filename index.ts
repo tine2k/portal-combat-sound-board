93160db872fcdf905b4b5da2d1b9ffb516dd58f7
tine2k <martin.maier.moessner@gmail.com>
2019-10-10 22:28:07 +0200

    3 files modified

--- index.ts
+++ index.ts
@@ -2,3 +2,15 @@
 import './style.css';
 
 // Write TypeScript code!
+function play(name) {
+  let audio = new Audio('https://raw.githubusercontent.com/tine2k/portal-combat-sound-board/master/' + name + '.mp3');
+  audio.addEventListener('play', () => document.getElementById(name).disabled = true);
+  audio.addEventListener('ended', () => document.getElementById(name).disabled = false);
+  audio.play();
+}
+
+function cronjob() {
+  console.log('scheduling job...');
+  var textSched = later.parse.text('every 10 seconds');
+  var timer2 = later.setInterval(() => play('standup'), textSched);
+};
